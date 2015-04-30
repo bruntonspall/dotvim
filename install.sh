a3ea20300e675b1c2645eddd1e464980b1e927d0
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2015-04-30 09:03:13 +0100

    Clone Vundle into the right directory

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ln -s ~/home/etc/dotvim/vimrc ~/.vimrc
-git clone https://github.com/gmarik/Vundle.vim.git bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git $(dirname $0)/bundle/Vundle.vim
 vim +PluginInstall +qall
